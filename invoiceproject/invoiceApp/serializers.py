aa252d9a6b017c9e70cebc7b548d817a6eec2ac2
muhwezideo <aggrey256@gmail.com>
2018-11-03 12:28:32 +0300

    add serializer for invoice model

---
+++ invoiceproject/invoiceApp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from invoiceApp.models import Invoice
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    # handles converting model to json format
+    class Meta:
+        model=Invoice
+        fields=('name','description','total','paid')
