09f92a786c4a20e35e9785254e8f2992b253ffb4
muhwezideo <aggrey256@gmail.com>
2018-11-03 12:45:11 +0300

    create view for get all invoices and register model in admin sectio

--- invoiceproject/invoiceApp/views.py
+++ invoiceproject/invoiceApp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from invoiceApp.models import Invoice
+from invoiceApp.serializers import InvoiceSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def invoice_list(request):
+    if request.method=="GET"
+    invoices=Invoice.objects.all()
+    serializer=InvoiceSerializer(invoices,many=True)
+    return Response(serializer.data)
